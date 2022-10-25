d89ee019e1d36f7566e08ba78e780c595271f75f
Abhishek Kanojia <abhishek14kanojia@gmail.com>
2022-10-25 23:19:11 +0530

    Contact

---
+++ Main.java
@@ -0,0 +1,10 @@
+package com.collection;
+import java.util.*;
+public class Main {
+
+	public static void main(String[] args) {
+		Contact contact = new Contact("Abhishek", "Kanojia", "Ujjain", "MadhyaPradesh", 456331, 8989232830L, "abc@gmail.com");
+		System.out.println(contact);
+	}
+
+}
